25d64153d61c1e196ed1a555bd89b94de4ac6c1d
Mohammed Arhan <mdarhan0309@gmail.com>
2025-07-06 03:57:04 +0530

    added

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const store = MongoStore.create({
   mongoUrl: Dburl,
   crypto: {
-    secret: "mykey",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -45,7 +45,7 @@ store.on("error", () => {
 
 const sessionOptions = {
   store,
-  secret: "mykey",
+  secret:process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
